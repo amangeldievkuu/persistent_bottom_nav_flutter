79f1705c85ada5e2bcfb7a5b80ed7108301a35a2
Kutman Amangeldiev <kutmanamangeldiev@Kutmans-MacBook-Pro.local>
2024-02-16 01:18:00 +0800

    bottom nav implementing

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'components/persistent_nav.dart';
 
 class MyAppView extends StatelessWidget {
   const MyAppView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      home: PersistentTabScreen(),
+    );
   }
 }
---
+++ lib/components/persistent_nav.dart
@@ -0,0 +1,85 @@
+import 'package:bottom_navigation_bar/screens/home/home.dart';
+import 'package:bottom_navigation_bar/screens/profile/profile.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:persistent_bottom_nav_bar_v2/persistent-tab-view.dart';
+
+class PersistentTabScreen extends StatefulWidget {
+  const PersistentTabScreen({super.key});
+
+  @override
+  State<PersistentTabScreen> createState() => _PersistentTabScreenState();
+}
+
+class _PersistentTabScreenState extends State<PersistentTabScreen> {
+  late PersistentTabController _controller;
+
+  List<Widget> _buildScreens() {
+    return [
+      const HomeScreen(),
+      const ProfileScreen(),
+    ];
+  }
+
+  List<PersistentBottomNavBarItem> _navBarsItems() {
+    return [
+      PersistentBottomNavBarItem(
+        icon: const FaIcon(FontAwesomeIcons.house),
+        title: "Home",
+        activeColorSecondary: Colors.white,
+        activeColorPrimary: Colors.blue,
+        inactiveColorPrimary: Colors.grey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: const FaIcon(FontAwesomeIcons.person),
+        title: "Settings",
+        activeColorSecondary: Colors.white,
+        activeColorPrimary: Colors.blue,
+        inactiveColorPrimary: Colors.grey,
+      ),
+    ];
+  }
+
+  @override
+  void initState() {
+    _controller = PersistentTabController(initialIndex: 0);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PersistentTabView(
+      context,
+      controller: _controller,
+      screens: _buildScreens(),
+      items: _navBarsItems(),
+      confineInSafeArea: true,
+      backgroundColor: Colors.white, // Default is Colors.white.
+      handleAndroidBackButtonPress: true, // Default is true.
+      resizeToAvoidBottomInset:
+          true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
+      stateManagement: true, // Default is true.
+      hideNavigationBarWhenKeyboardShows:
+          true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
+      decoration: NavBarDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        colorBehindNavBar: Colors.white,
+      ),
+      popAllScreensOnTapOfSelectedTab: true,
+      popActionScreens: PopActionScreensType.all,
+      itemAnimationProperties: const ItemAnimationProperties(
+        // Navigation Bar's items animation properties.
+        duration: Duration(milliseconds: 300),
+        curve: Curves.ease,
+      ),
+      screenTransitionAnimation: const ScreenTransitionAnimation(
+        // Screen transition animation on change of selected tab.
+        animateTabTransition: true,
+        curve: Curves.ease,
+        duration: Duration(milliseconds: 300),
+      ),
+      navBarStyle:
+          NavBarStyle.style7, // Choose the nav bar style with this property.
+    );
+  }
+}
